cb37d4b8e4ad9c470eded8f03cdc60f1c31edb35
Luyanda-Glucode <117176161+Luyanda-Glucode@users.noreply.github.com>
2023-02-20 09:39:38 +0200

    registered a viewController and added .gradle folder to git ignore

--- src/main/java/com/msrvtasks/springboottasks/SpringFoxConfig.java
+++ src/main/java/com/msrvtasks/springboottasks/SpringFoxConfig.java
@@ -1,16 +1,17 @@
 package com.msrvtasks.springboottasks;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
@@ -20,13 +21,19 @@ public class SpringFoxConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
-    
+
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry){
+          registry.addRedirectViewController("/docApi/v2/api-docs","/v2/api-docs");
+        registry.addViewController("/HelloWorld").setViewName("HelloWorld");
     }
 }
 
